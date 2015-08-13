fda1cefeac2494e1a8fb124e1d045a65a2755587
leito9008 <leito.vm3@hotmail.com>
2015-08-13 15:53:41 -0300

    Configurable image replace path and extension

--- check-svg.js
+++ check-svg.js
@@ -2,30 +2,52 @@
 
 // Declare the module
 var checksvg = checksvg || (function () {
-    'use strict';
+    "use strict";
 
     var truechecksvg = truechecksvg || {};
 
+    // If the png folder path is not manually set, it takes
+    // the svg current path
+    truechecksvg.pngFolderPath = "";
+
+    // Sets .png as default image extension replace
+    truechecksvg.replaceImageFormat = ".png";
+
     function getFileExtension(imageSrc) {
-        var extension = imageSrc.split('.');
+        var extension = imageSrc.split(".");
 
         return extension[extension.length - 1];
     }
 
+    function _doGetFileFullPath(currentImageSrc, pngFolderPath) {
+        currentImageSrc = currentImageSrc.replace(".svg", truechecksvg.replaceImageFormat);
+        var splitResult = currentImageSrc.split("/");
+        var imageFileName = splitResult[splitResult.length - 1];
+
+        // If an specific folder is set, make and return the full path url
+        if(pngFolderPath !== "") {
+            return pngFolderPath + imageFileName;
+        }
+
+        // If not set, just return the current svg path with changed extension
+        return currentImageSrc;
+
+    }
+
     function _doSupportsSvg() {
-        return !document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+        return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
     }
 
     function _doReplaceSvgWithPng() {
         return new Promise(function (resolve, reject) {
-            var documentImages = document.getElementsByTagName('img');
+            var documentImages = document.getElementsByTagName("img");
+            var pngFolderPath = truechecksvg.pngFolderPath || "";
 
             for (var i = 0; i < documentImages.length; i++) {
                 var currentImage = documentImages[i];
-                var currentImageSrc = currentImage.getAttribute('src');
+                var currentImageSrc = currentImage.getAttribute("src");
                 if (getFileExtension(currentImageSrc) === "svg") {
-                    var replacedSrc = currentImageSrc.replace('.svg', '.' + 'png');
-                    currentImage.setAttribute('src', replacedSrc);
+                    currentImage.setAttribute("src", _doGetFileFullPath(currentImageSrc, pngFolderPath));
                 }
             }
         });
@@ -47,6 +69,16 @@ var checksvg = checksvg || (function () {
 
     truechecksvg.checkAndReplace = function () {
         return _doCheckAndReplace();
+    };
+
+    // The full folder path from the root of the project
+    truechecksvg.setPngFolderPath = function (folderPath) {
+        this.pngFolderPath = folderPath;
+    };
+
+    // .jpg, .gif, .png or whatever
+    truechecksvg.setReplaceImageFormat = function (imageFormat) {
+        this.replaceImageFormat = imageFormat;
     };
 
     return truechecksvg;
